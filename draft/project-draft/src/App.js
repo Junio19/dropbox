97faac975c035ae3b0224b2ac086364029fd8c23
Junio19 <johnpatrickbruzovalencia@gmail.com>
2023-01-12 15:36:26 +0800

    Props
    demonstrate how to use propr and destructuring

--- draft/project-draft/src/App.js
+++ draft/project-draft/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Demonstrate how to use props and destructuring 
+const Person = ({name, job, company})=>{
+  return(
+    <div>
+      <h1>Name: {name}</h1>
+      <h1>job: {job}</h1>
+      <h1>company: {company}</h1>
+    </div>
+  )
+}
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name='Jaypee' job='Software Engineer' company='Novare'/>
+      <Person />
     </div>
   );
 }
